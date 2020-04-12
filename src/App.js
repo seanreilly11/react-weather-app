d90aacd3ee73deea83d2cadc94b904dd104bf001
Sean <seanreilly123@hotmail.com>
2020-04-13 01:17:06 +1200

    Added weather icon and high/low

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-import React, {useState} from 'react';
-import './index.css';
+import React, { useState } from "react";
+import "./index.css";
 
 const api = {
   key: "79f0172b1b48d3921c1cb1e091dde288",
-  base: "https://api.openweathermap.org/data/2.5/weather"
-}
+  base: "https://api.openweathermap.org/data/2.5/weather",
+};
 
 function App() {
-  const [query,setQuery] = useState("");
-  const [weather,setWeather] = useState({});
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
-  const search = e => {
-    if (e.key === "Enter"){
+  const search = (e) => {
+    if (e.key === "Enter") {
       fetch(`${api.base}?q=${query}&units=metric&appid=${api.key}`)
-      .then(res => res.json())
-      .then(result => {
-        setWeather(result)
-        setQuery("");
-        console.log(result)
-      });
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
     }
-  }
+  };
 
   const dateBuilder = (d) => {
     const MONTHS = [
-      'January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December'
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
     ];
     const DAYS = [
-      'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
     ];
 
     let day = DAYS[d.getDay()];
@@ -37,28 +53,50 @@ function App() {
     let year = d.getFullYear();
 
     return `${day} ${date} ${month} ${year}`;
-  }
+  };
   return (
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
       <main>
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="Search..." onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search}/>
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-        {(typeof weather.main != "undefined") ? (
+        {typeof weather.main != "undefined" ? (
           <div>
             <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
               <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="temps">
+                <div className="temp-min">{Math.round(weather.main.temp_min)}°c</div>             
+                <div className="temp-max">{Math.round(weather.main.temp_max)}°c</div>
+              </div>
+              <img className="icon" src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}/>
               <div className="weather">{weather.weather[0].main}</div>
             </div>
           </div>
         ) : (
           <div className="logo">Beautiful Day Weather App</div>
         )}
-      </main>    
+      </main>
     </div>
   );
 }
